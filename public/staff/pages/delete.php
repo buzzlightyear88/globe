eb8a72f68df5c5c0971c90d1c18fbc6c96250981
Panos <panagiotis.rapo@gmail.com>
2018-03-25 14:53:55 -0400

    count pages inside subject index

--- public/staff/pages/delete.php
+++ public/staff/pages/delete.php
@@ -7,17 +7,14 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/pages/index.php'));
 }
 $id = $_GET['id'];
+$page = find_page_by_id($id);
 
 if(is_post_request()) {
 
   $result = delete_page($id);
   $_SESSION['message'] = 'The page was deleted successfully.';
-  redirect_to(url_for('/staff/pages/index.php'));
-
-} else {
-  $page = find_page_by_id($id);
+  redirect_to(url_for('/staff/subjects/show.php=' . h(u($page['subject_id']))));
 }
-
 ?>
 
 <?php $page_title = 'Delete Page'; ?>
